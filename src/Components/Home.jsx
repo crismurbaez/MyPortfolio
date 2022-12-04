17b9d641dc730e5e1e5480d2747313fbcc152149
Cristina <clmbjcm@gmail.com>
2022-12-04 18:53:02 -0300

    screen

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
 
 
     return (
-        <div className=" bg-gray-900 h-full w-full">
+        <div className=" bg-gray-900 sm:h-full lg:h-screen w-full">
             <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
                 <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                     <div className="sm:max-w-lg">
