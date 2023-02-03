f9b9e88a5dbc15448fdd0391fc7a5265d6ad56a0
Cristina <clmbjcm@gmail.com>
2023-02-03 12:19:17 -0300

    delete line duplicated for contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -90,8 +90,7 @@ export default function Contact() {
                 <div className=" bg-gray-900  pb-20">
                     <div className=" bg-gray-900">
                         <div className=" px-4 sm:px-14 bg-gray-900">
-                            <h2 className="text-6xl font-bold tracking-tight text-orange-200 px-4">Contacto</h2>
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">Contacto</h3>
+                            <h2 className="text-6xl font-bold tracking-tight text-orange-200 p-4">Contacto</h2>
                             <div className='flex flex-col sm:flex-row'>
                                 <p className=" px-4 text-sm  text-orange-200">
                                     Me encuentras en:
