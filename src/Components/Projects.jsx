6a1bbfb5da075858f94cce0f686efb01d9fedae8
Cristina <clmbjcm@gmail.com>
2023-11-15 11:54:14 -0300

    fix add button video

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -187,7 +187,7 @@ export default function Projects() {
                                         </a>
                                     </div>
 
-                                    : null}
+                                    : <div></div>}
 
 
 
